7783249753c53ae5b3f6ffa38cc10cf0c60e3753
David Dobrynin <David.Dobrynin@gmail.com>
2017-04-17 15:38:30 -0700

    Creates the data layer

--- first_routes/db/seeds.rb
+++ first_routes/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ActiveRecord::Base.transaction do
+  User.destroy_all
+  u1 = User.create(username: 'dobrynin')
+  u2 = User.create(username: 'sawatske')
+
+  Artwork.destroy_all
+  a1 = Artwork.create(image_url: 'https://upload.wikimedia.org/wikipedia/en/5/58/Interchange_%28de_Kooning%29.jpg',
+                      title: 'Interchange', artist_id: u1.id)
+  a2 = Artwork.create(image_url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Card_Players-Paul_Cezanne.jpg/1024px-Card_Players-Paul_Cezanne.jpg',
+                    title: 'The Card Players', artist_id: u2.id)
+
+  ArtworkShare.destroy_all
+  as1 = ArtworkShare.create(artwork_id: a1.id, viewer_id: u2.id)
+  as2 = ArtworkShare.create(artwork_id: a2.id, viewer_id: u1.id)
+end
